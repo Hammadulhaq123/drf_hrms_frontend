da3d2bcf2ac3c9e6fe3bea435f1188d4ae20f7c8
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-11 02:07:53 +0500

    Styling Updated

--- src/employee/components/EmployeeWelcome.jsx
+++ src/employee/components/EmployeeWelcome.jsx
@@ -3,12 +3,14 @@ import { AppContext } from '../../context/Context'
 
 const EmployeeWelcome = () => {
 
-    const { theme, primary_bg } = useContext(AppContext);
+    const { theme, primary_text } = useContext(AppContext);
 
 
     return (
-        <div className={`w-full h-32 p-4 flex justify-start items-center ${primary_bg} rounded-md`}>
-
+        <div className={`w-full h-auto p-4 flex justify-start items-center ${primary_text} rounded-md`}>
+            <span className='text-5xl font-bold'>
+                Welcome JackAnderson
+            </span>
         </div>
     )
 }
