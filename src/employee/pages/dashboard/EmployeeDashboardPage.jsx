6fd5161adba3068aca13d62645b7fc3a113d8286
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-10 18:25:15 +0500

    Update: Used the created components in the Dashboard Page

--- src/employee/pages/dashboard/EmployeeDashboardPage.jsx
+++ src/employee/pages/dashboard/EmployeeDashboardPage.jsx
@@ -7,34 +7,27 @@ import { AppContext } from "../../../context/Context";
 import EmployeeAnnounements from "../../components/EmployeeAnnounements";
 import EmployeeLeaves from "../../components/EmployeeLeaves";
 import EmployeeTodo from "../../components/EmployeeTodo";
+import EmployeeWelcome from "../../components/EmployeeWelcome";
+import EmployeeStats from "../../components/EmployeeStats";
 
 const EmployeeDashboardPage = () => {
   const { theme, dark_contrast, light_contrast } = useContext(AppContext);
 
   return (
-    <div className="w-full lg:w-[calc(100%-18rem)] overflow-y-auto relative flex flex-col gap-3 h-full">
+    <div className="w-full lg:w-[calc(100%-15rem)] overflow-y-auto relative flex flex-col gap-3 h-full">
       <EmployeeNavbar />
-      <div className="w-full absolute top-16 left-0 p-4 h-auto ">
-        <Swiper
-          spaceBetween={20}
-          slidesPerView={window.innerWidth < 800 ? 1 : 3}
-        >
-          <SwiperSlide className={` h-[30rem] ${
-        theme == "dark" ? dark_contrast : light_contrast
-      } rounded-xl`}>
-            <EmployeeAnnounements />
-          </SwiperSlide>
-          <SwiperSlide className={` h-[30rem] ${
-        theme == "dark" ? dark_contrast : light_contrast
-      } rounded-xl`}>
-            <EmployeeTodo />
-          </SwiperSlide>
-          <SwiperSlide className={` h-[30rem] ${
-        theme == "dark" ? dark_contrast : light_contrast
-      } rounded-xl`}>
-            <EmployeeLeaves />
-          </SwiperSlide>
-        </Swiper>
+
+      <div className="w-full absolute top-16 left-0 p-4 h-auto flex flex-col justify-start items-start gap-4">
+
+        <EmployeeWelcome />
+
+        <EmployeeStats />
+
+        <div className="w-full  h-auto flex flex-col flex-wrap sm:flex-row gap-4">
+          <EmployeeAnnounements />
+          <EmployeeLeaves />
+          <EmployeeTodo />
+        </div>
       </div>
     </div>
   );
