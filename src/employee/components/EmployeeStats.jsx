d7adf8f9b9f17e5754eebc3585984e344dbdf780
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-11 02:07:38 +0500

    Update: Used the component of Employee stats

--- src/employee/components/EmployeeStats.jsx
+++ src/employee/components/EmployeeStats.jsx
@@ -1,15 +1,32 @@
 import React, { useContext } from 'react'
 import EmployeeAttendanceGraph from './EmployeeAttendanceGraph'
+import { Link } from "react-router-dom";
 import EmployeeCheckInOut from './EmployeeCheckInOut'
 import { AppContext } from '../../context/Context'
+import EmployeeMonthlyStatsPieChart from './EmployeeMonthlyStatsPieChart'
+import { MdOutlineArrowOutward } from 'react-icons/md';
 
 const EmployeeStats = () => {
 
-    const { theme, dark_contrast, light_contrast } = useContext(AppContext);
+    const { theme, dark_contrast, light_contrast, dark_background, primary_bg } = useContext(AppContext);
 
     return (
-        <div className={`w-full h-72 flex justify-start items-start rounded-md ${theme == 'dark' ? dark_contrast : light_contrast}`}>
-            <EmployeeAttendanceGraph />
+        <div className={`w-full h-96 flex flex-col justify-start items-start gap-3  rounded-md `}>
+            <div className={`w-full h-[15%] flex items-center justify-between px-6 ${theme == 'dark' ? dark_contrast : light_contrast} text-2xl font-bold rounded-md`}>
+                <span>
+                    Attendance Stats
+                </span>
+
+                <Link to="/employee/auth/login/" className={`w-8 h-8 rounded-md flex items-center justify-center ${primary_bg}`}>
+                    <MdOutlineArrowOutward className='text-xl' />
+
+                </Link>
+            </div>
+            <div className='w-full h-[85%] flex flex-row gap-3 justify-start items-start'>
+                <EmployeeAttendanceGraph />
+                <EmployeeMonthlyStatsPieChart />
+
+            </div>
         </div>
     )
 }
