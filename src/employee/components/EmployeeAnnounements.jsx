3705d2e1ed787d75097d91605fd2cdd91ce468dc
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-11 02:08:10 +0500

    Used the components and structured them

--- src/employee/components/EmployeeAnnounements.jsx
+++ src/employee/components/EmployeeAnnounements.jsx
@@ -1,15 +1,55 @@
 import React, { useContext } from "react";
 import { SwiperSlide } from "swiper/react"
 import { AppContext } from "../../context/Context";
+import EmployeeAnnouncementHeader from "./EmployeeAnnouncementHeader";
+import Announcement from "../../components/Announcement";
 
 
 
 const EmployeeAnnounements = () => {
 
   const { theme, dark_contrast, light_contrast } = useContext(AppContext);
+
+  const dummyArr = [
+    {
+      profile: "https://randomuser.me/api/portraits/men/86.jpg",
+      announcement: 'Saturday is on just for this week',
+      keyword: 'General'
+    },
+    {
+      profile: "https://randomuser.me/api/portraits/men/86.jpg",
+      announcement: 'Saturday is on just for this week',
+      keyword: 'General'
+    },
+    {
+      profile: "https://randomuser.me/api/portraits/men/86.jpg",
+      announcement: 'Saturday is on just for this week',
+      keyword: 'General'
+    },
+    {
+      profile: "https://randomuser.me/api/portraits/men/86.jpg",
+      announcement: 'Saturday is on just for this week',
+      keyword: 'General'
+    },
+  ]
   return (
 
-    <div className={`w-full sm:w-[48%] lg:w-[31%] xl:w-[32.2%]  ${theme == 'dark' ? dark_contrast : light_contrast} h-[30rem] rounded-md`}>
+    <div className={`w-full sm:w-[48.5%] xl:w-[40%]   ${theme == 'dark' ? dark_contrast : light_contrast} h-[30rem] xl:h-[26rem] flex flex-col gap-4 justify-start items-start rounded-md px-4 py-2`}>
+
+      {/* Header for employees */}
+      <EmployeeAnnouncementHeader />
+
+      {/* Few Announcements Mapping */}
+      <div className="w-full h-[90%] flex flex-col gap-2 justify-start items-start">
+
+        {
+          dummyArr.splice(0, 4).map((announcement, key) => {
+            return (
+              <Announcement key={key} announcement={announcement.announcement} keyword={announcement.keyword} profile={announcement.profile} />
+            )
+          })
+        }
+      </div>
 
     </div>
   );
