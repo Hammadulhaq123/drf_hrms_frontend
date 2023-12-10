5eabbbaa326a9aebb63b186432cb5371c054ecc6
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-10 18:20:44 +0500

    Sidebar toggle functionality added

--- src/employee/components/EmployeeSidebar.jsx
+++ src/employee/components/EmployeeSidebar.jsx
@@ -1,20 +1,20 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { AppContext } from '../../context/Context'
 import { FaRegUser } from "react-icons/fa";
 import { LuHome } from "react-icons/lu";
 import { GrAnnounce } from "react-icons/gr";
 import EmployeeSidebarLink from './EmployeeSidebarLink';
-
+import { IoIosArrowBack } from "react-icons/io";
 
 const EmployeeSidebar = () => {
 
-    const {theme, dark_contrast, light_contrast, primary_bg, semi_dark_text_color, semi_light_text_color, primary_text} = useContext(AppContext);
+    const { theme, dark_contrast, light_contrast, primary_bg, dark_background, light_background, semi_dark_text_color, semi_light_text_color, primary_text } = useContext(AppContext);
 
     const iconArr = [
         {
             'icon': <LuHome />,
             'title': 'Home',
-            'href' : '/employee/dashboard/'
+            'href': '/employee/dashboard/'
         },
         {
             'icon': <FaRegUser />,
@@ -28,33 +28,44 @@ const EmployeeSidebar = () => {
         },
     ]
 
+    const closeSidebar = () => {
+        const sidebar = document.getElementById("sidebar");
+        sidebar.classList.add("-translate-x-full");
+        sidebar.classList.remove("translate-x-0")
+    }
 
-  return (
-    <div className={`absolute top-0 left-0 lg:static  py-4 flex flex-col pr-2 transition-all duration-150 gap-1 ${theme == 'dark' ? dark_contrast : light_contrast} -translate-x-full lg:translate-x-0  w-72 h-full`}>
+    return (
+        <div id='sidebar' className={`absolute top-0 left-0 lg:static  py-4 flex flex-col pr-2 transition-all duration-150 gap-1 ${theme == 'dark' ? dark_contrast : light_contrast} -translate-x-full z-50 lg:translate-x-0  w-60 h-full`}>
 
-        <div className={`w-full px-5 h-auto text-3xl font-extrabold  text-left`}>
-            HRMS
-        </div>
-        <div className='w-full h-20 flex justify-start px-4 gap-3 items-center'>
-            <img src="https://randomuser.me/api/portraits/men/58.jpg" alt="profile" className='w-10 h-10 rounded-full'/>
-            <div className='w-auto flex flex-col justify-start items-start'>
-                <span className={`text-lg font-bold ${primary_text}`}>
-                    Jack Anderson
+            <div className={`w-full px-5 h-auto  flex justify-between items-center font-extrabold  text-left`}>
+                <span className='text-3xl'>
+                    HRMS
                 </span>
-                <span className={`text-sm font-medium ${theme == 'dark' ? semi_dark_text_color : semi_light_text_color}`}>
-                    Web Developer
-                </span>
+                <button onClick={closeSidebar} className={`outline-none lg:hidden block border-none ${theme == 'dark' ? dark_background : light_background} rounded-full w-6 h-6 flex justify-center items-center`}>
+
+                    <IoIosArrowBack className='text-xl ' />
+                </button>
             </div>
+            <div className='w-full h-20 flex justify-start px-4 gap-3 items-center'>
+                <img src="https://randomuser.me/api/portraits/men/58.jpg" alt="profile" className='w-10 h-10 rounded-full' />
+                <div className='w-auto flex flex-col justify-start items-start'>
+                    <span className={`text-lg font-bold ${primary_text}`}>
+                        Jack Anderson
+                    </span>
+                    <span className={`text-sm font-medium ${theme == 'dark' ? semi_dark_text_color : semi_light_text_color}`}>
+                        Web Developer
+                    </span>
+                </div>
+            </div>
+            {
+                iconArr.map((obj, key) => {
+                    return (
+                        <EmployeeSidebarLink key={key} icon={obj.icon} title={obj.title} />
+                    )
+                })
+            }
         </div>
-        {
-            iconArr.map((obj, key)=> {
-                return(
-                    <EmployeeSidebarLink key={key} icon={obj.icon} title={obj.title} />
-                )
-            })
-        }
-    </div>
-  )
+    )
 }
 
 export default EmployeeSidebar
--- src/employee/components/EmployeeSidebarLink.jsx
+++ src/employee/components/EmployeeSidebarLink.jsx
@@ -2,25 +2,25 @@ import React, { useContext, useState } from 'react'
 import { AppContext } from '../../context/Context'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
-const EmployeeSidebarLink = ({icon, title, href}) => {
+const EmployeeSidebarLink = ({ icon, title, href }) => {
 
-    
-    const {theme, dark_background, light_background, dark_contrast, primary_text, primary_bg} = useContext(AppContext)
 
-    const navigate = useNavigate()
+  const { theme, dark_background, light_background, dark_contrast, primary_text, primary_bg } = useContext(AppContext)
 
-    
+  const navigate = useNavigate()
 
-    
+
+
+
   return (
-    <Link to={href} className={`sidebar_links group w-[100%] h-12 rounded-r-full transition-all  duration-150 text-md flex items-center justify-start gap-4 ${theme =='dark' ? 'focus-within:bg-[#000]' : 'focus-within:bg-[#fff]' } ${theme == 'dark' ? 'hover:bg-[#000]' : 'hover:bg-[#fff]'} hover:opacity-80`}>
-        <span className={`group-hover:bg-[#005AA7] group-focus:bg-[#005AA7] w-1 h-full`}/>
-        <span className={`${theme == 'dark' ? dark_background : light_background}  w-8 h-8 flex justify-center items-center rounded-full`}>
+    <Link to={href} className={`sidebar_links group w-[100%] h-12 rounded-r-full transition-all  duration-150 text-md flex items-center justify-start gap-4 ${theme == 'dark' ? 'focus-within:bg-[#000]' : 'focus-within:bg-[#fff]'} ${theme == 'dark' ? 'hover:bg-[#000]' : 'hover:bg-[#fff]'} hover:opacity-80`}>
+      <span className={`group-hover:bg-gradient-to-r group-hover:from-[#8A2387] group-hover:via-[#E94057] group-hover:to-[#F27121] group-focus:bg-gradient-to-r group-focus:from-[#8A2387] group-focus:via-[#E94057] group-focus:to-[#F27121] w-1 h-full`} />
+      <span className={`${theme == 'dark' ? dark_background : light_background}  w-8 h-8 flex justify-center items-center rounded-full`}>
         {icon}
-        </span>
-        <span className={`text-md font-medium`}>
-            {title}
-        </span>
+      </span>
+      <span className={`text-md font-medium`}>
+        {title}
+      </span>
     </Link>
   )
 }
