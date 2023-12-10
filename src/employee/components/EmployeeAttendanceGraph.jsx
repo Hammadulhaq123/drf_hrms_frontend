1ffca0cbe1be8c356ff73ccc8b9bc7215e95aa4c
Hammadulhaq123 <professorcoding123@gmail.com>
2023-12-11 02:05:35 +0500

    Update: Graph styling updated

--- src/employee/components/EmployeeAttendanceGraph.jsx
+++ src/employee/components/EmployeeAttendanceGraph.jsx
@@ -7,50 +7,175 @@ const EmployeeAttendanceGraph = () => {
 
     const data = [
         {
-            name: 'Day 1',
+            name: '1',
             DT: 9,
             OT: 0,
         },
         {
-            name: 'Day 2',
+            name: '2',
             OT: 2,
             DT: 9,
         },
         {
-            name: 'Day 3',
+            name: '3',
             OT: 4,
             DT: 9,
         },
         {
-            name: 'Day 4',
+            name: '4',
             OT: 3,
             DT: 9,
         },
         {
-            name: 'Day 5',
+            name: '5',
             OT: 0,
             DT: 8,
         },
         {
-            name: 'Day 6',
+            name: '6',
+            OT: 7,
+            DT: 9,
+        },
+        {
+            name: '7',
+            DT: 9,
+            OT: 0,
+        },
+        {
+            name: '8',
+            OT: 2,
+            DT: 9,
+        },
+        {
+            name: '9',
+            OT: 4,
+            DT: 9,
+        },
+        {
+            name: '10',
+            OT: 3,
+            DT: 9,
+        },
+        {
+            name: '11',
+            OT: 0,
+            DT: 8,
+        },
+        {
+            name: '12',
+            OT: 7,
+            DT: 9,
+        },
+        {
+            name: '13',
+            DT: 9,
+            OT: 0,
+        },
+        {
+            name: '14',
+            OT: 2,
+            DT: 9,
+        },
+        {
+            name: '15',
+            OT: 4,
+            DT: 9,
+        },
+        {
+            name: '16',
+            OT: 3,
+            DT: 9,
+        },
+        {
+            name: '17',
+            OT: 0,
+            DT: 8,
+        },
+        {
+            name: '18',
+            OT: 7,
+            DT: 9,
+        },
+        {
+            name: '19',
+            DT: 9,
+            OT: 0,
+        },
+        {
+            name: '20',
+            OT: 2,
+            DT: 9,
+        },
+        {
+            name: '21',
+            OT: 4,
+            DT: 9,
+        },
+        {
+            name: '22',
+            OT: 3,
+            DT: 9,
+        },
+        {
+            name: '23',
+            OT: 0,
+            DT: 8,
+        },
+        {
+            name: '24',
+            OT: 7,
+            DT: 9,
+        },
+        {
+            name: '25',
+            OT: 1,
+            DT: 9,
+        },
+        {
+            name: '26',
+            OT: 7,
+            DT: 9,
+        },
+        {
+            name: '27',
+            OT: 4,
+            DT: 9,
+        },
+        {
+            name: '28',
+            OT: 1,
+            DT: 9,
+        },
+        {
+            name: '29',
+            OT: 3,
+            DT: 9,
+        },
+        {
+            name: '30',
+            OT: 2,
+            DT: 9,
+        },
+        {
+            name: '31',
             OT: 7,
             DT: 9,
         },
 
     ];
 
-    const { theme } = useContext(AppContext)
+    const { theme, dark_background, light_background, dark_contrast, light_contrast } = useContext(AppContext)
 
     return (
-        <div className='w-full h-full pt-10 px-8'>
+        <div className={`w-1/2 lg:w-[80%] h-full  flex items-center md:pt-8 md:pr-8 rounded-md justify-center ${theme == 'dark' ? dark_contrast : light_contrast}`}>
             <ResponsiveContainer wiOTh="100%" height="100%">
                 <BarChart
-                    wiOTh={500}
-                    height={300}
+                    width={100}
+                    height={200}
                     data={data}
 
                 >
-                    <CartesianGrid />
+                    {/* <CartesianGrid /> */}
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
